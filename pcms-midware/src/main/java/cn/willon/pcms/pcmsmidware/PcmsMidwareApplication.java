48c73c7ffba409f3a6c132c24b9f5841094f8430
willon <willon295@gmail.com>
2019-04-07 09:17:54 +0800

    enable  feign

--- pcms-midware/src/main/java/cn/willon/pcms/pcmsmidware/PcmsMidwareApplication.java
+++ pcms-midware/src/main/java/cn/willon/pcms/pcmsmidware/PcmsMidwareApplication.java
@@ -4,6 +4,7 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
  * PcmsMidwareApplication
@@ -13,6 +14,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
  */
 @SpringBootApplication
 @EnableEurekaClient
+@EnableFeignClients
 @MapperScan(basePackages = "cn.willon.pcms.pcmsmidware.mapper")
 public class PcmsMidwareApplication {
 
